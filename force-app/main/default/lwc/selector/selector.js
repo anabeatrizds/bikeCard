e96de62ffc060fbc663e2e0e95ad487fa63c1c39
Ana Beatriz dos Santos <61064552+anabeatrizds@users.noreply.github.com>
2021-06-01 14:00:11 -0300

    Add files via upload

---
+++ force-app/main/default/lwc/selector/selector.js
@@ -0,0 +1,14 @@
+import { LightningElement, wire } from 'lwc'; import { getRecord, getFieldValue } from 'lightning/uiRecordApi'; import Id from '@salesforce/user/Id'; import NAME_FIELD from '@salesforce/schema/User.Name'; const fields = [NAME_FIELD];
+
+export default class Selector extends LightningElement {
+    name;
+    Name(evt) {
+        this.name = evt.detail;
+    }
+    userId = Id;
+    @wire(getRecord, { recordId: '$userId', fields })
+    user;
+    get name() {
+        return getFieldValue(this.user.data, NAME_FIELD);
+    }
+}
